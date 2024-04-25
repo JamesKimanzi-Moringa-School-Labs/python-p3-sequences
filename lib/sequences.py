5a2781bb49c36490e73049d628e653d0e5b1a060
James-Kimanzi <jameskimanzi@gmail.com>
2024-04-25 05:55:22 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length):   
+    fibonacci = [0, 1]
+    if length <= 0:
+        print ([])
+    elif length == 1:
+        print([0])
+        
+    elif length == 2:
+        print([0, 1])
+        
+    else:
+        for i in range(2, length):
+            fibonacci_next = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(fibonacci_next)
+    return fibonacci
+print(print_fibonacci(10))
+
+
